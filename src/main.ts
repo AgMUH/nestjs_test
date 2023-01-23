41fcd4e70d2508c5696470fb4cac79e46450583c
vkotliarenko <vlad.kotlyarenko0@gmail.com>
2023-01-23 08:12:00 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   app.enableCors({ credentials: true, origin: true });
 
   await app.listen(3002);
