65c7d8e7af4f456f0023214bf44a9ff7c6d96979
vkotliarenko <vlad.kotlyarenko0@gmail.com>
2023-01-23 09:48:08 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: true });
   await app.listen(process.env.PORT || 5000 || 80);
 }
 bootstrap();
